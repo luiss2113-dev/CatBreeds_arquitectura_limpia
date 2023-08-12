3b51cad8539134222a155fc24c1a455153c08984
Luis Angel Ibañez Barbosa <62814020+luiss2113-dev@users.noreply.github.com>
2023-08-11 19:33:43 -0500

    Update constants.dart

--- lib/app/data/helpers/constants.dart
+++ lib/app/data/helpers/constants.dart
@@ -3,7 +3,7 @@ class Constants {
 
   static const String BASE_URL = "https://api.thecatapi.com/v1";
   static const String images = 'https://cdn2.thecatapi.com/images/';
-  static const String API_KEY = 'bda53789-d59e-46cd-9bc4-2936630fde39';
+  static const String API_KEY = 'YOUR-API-KEY';
 
   static const String logo = 'assets/images/logo.png';
   static const String errorImage = 'assets/images/cat_error.jpg';
